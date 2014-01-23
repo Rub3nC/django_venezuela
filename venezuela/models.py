ba416a4c038167f5dbfbcf36795d3892955b4dd5
Ruben Colina <ruben.colina@gmail.com>
2014-01-23 10:33:44 -0430

    se agrego el modelo ciudades y su migracion

--- venezuela/models.py
+++ venezuela/models.py
@@ -11,3 +11,16 @@ class Estado(models.Model):
     class Meta:
         verbose_name = u'Estado'
         verbose_name_plural = u'Estados'
+
+
+class Ciudad(models.Model):
+    estado = models.ForeignKey('Estado')
+    ciudad = models.CharField(max_length=100)
+    capital = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.ciudad
+
+    class Meta:
+        verbose_name = u'Ciudad'
+        verbose_name_plural = u'Ciudades'
